caa063cf4861d8690f3260ac77cbbfecc30d2fad
Hampi Rares <hampimarian1323@gmail.com>
2022-12-04 13:13:21 +0200

    am terminat functionalitatea navbarului

--- frontend/app.js
+++ frontend/app.js
@@ -1,9 +1,16 @@
 const login = document.getElementById("login");
 const register = document.getElementById("register");
 const page = document.getElementById("page");
+const logare = document.getElementById("btn_login");
+const unlog = document.getElementById("dec");
+const log = document.getElementById("log");
+const reg = document.getElementById("reg");
+
+//localStorage.setItem("Status", "Delogat");
 
 function openLogin() {
   login.style.display = "block";
+  register.style.display = "none";
   page.classList.add("page");
 }
 function closeLogin() {
@@ -12,9 +19,31 @@ function closeLogin() {
 }
 function openRegister() {
   register.style.display = "block";
+  login.style.display = "none";
   page.classList.add("page");
 }
 function closeRegister() {
   register.style.display = "none";
   page.classList.remove("page");
+}
+
+function Login() {
+  localStorage.setItem("Status", "Logat");
+}
+
+function disconect() {
+  localStorage.setItem("Status", "Delogat");
+}
+
+const myTime = setInterval(check, 100);
+function check() {
+  if (localStorage.getItem("Status") == "Logat") {
+    unlog.style.display = "block";
+    log.style.display = "none";
+    reg.style.display = "none";
+  } else {
+    unlog.style.display = "none";
+    log.style.display = "block";
+    reg.style.display = "block";
+  }
 }
